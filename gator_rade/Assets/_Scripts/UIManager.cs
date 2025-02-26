8359b808a42d7faa24df258386f71e7090ab8b05
Jiokester <jiokerules@gmail.com>
2025-02-26 12:02:15 -0800

    Added some level selection for the game and a main menu

--- gator_rade/Assets/_Scripts/UIManager.cs
+++ gator_rade/Assets/_Scripts/UIManager.cs
@@ -3,16 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
+/*
+ * Author: [Ihejirika, Chijioke]
+ * Last Updated: [02/26/2025]
+ * [Handles the changing and restarting of scenes in the game]
+ */
+
 
 public class UIManager : MonoBehaviour
 {
-    public TMP_Text levelText;
 
+    //public TMP_Text levelText;
+    public int currentScene;
 
-    private void Update()
+    private void Start()
     {
-      //  levelText = SceneManager.
+        //currentScene = SceneManager.;
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene(2);
       
+    }
+
+    public void Level()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Level1Select()
+    {
+        SceneManager.LoadScene(2);
+
+    }
+
+    public void Level2Select()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void RestartLevel1()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void RestartLevel2()
+    {
+        SceneManager.LoadScene(3);
     }
 
 }
