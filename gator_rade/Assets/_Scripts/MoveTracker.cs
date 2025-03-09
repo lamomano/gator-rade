1b5ae879552d63101516cb9a0c02a98e4f3c82fc
Jiokester <jiokerules@gmail.com>
2025-03-09 12:50:49 -0700

    Added a moveTracker to the playetest scene

--- gator_rade/Assets/_Scripts/MoveTracker.cs
+++ gator_rade/Assets/_Scripts/MoveTracker.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class MoveTracker : MonoBehaviour
 {
@@ -8,10 +11,14 @@ public class MoveTracker : MonoBehaviour
     private GameGrid gameGrid;
     private GameManager gameManager;
 
+    public bool tokenDestroyed;
+
+    public TextMeshProUGUI currentMovesLeft;
     private void Awake()
     {
         gameGrid = (GameGrid)FindObjectOfType<GameGrid>();
         gameManager = (GameManager)FindObjectOfType<GameManager>();
+        currentMovesLeft.text = gameManager.MOVES_LEFT.ToString();
     }
 
 
@@ -20,6 +27,16 @@ public class MoveTracker : MonoBehaviour
     /// </summary>
     public void OnMove()
     {
-        
+        if(tokenDestroyed == true)
+        {
+            gameManager.MOVES_LEFT--;
+            gameManager.amountOfMoves = gameManager.MOVES_LEFT;
+            currentMovesLeft.text = gameManager.MOVES_LEFT.ToString();
+
+            if(gameManager.MOVES_LEFT == 0)
+            {
+                Debug.Log("You lose");
+            }
+        }
     }
 }
