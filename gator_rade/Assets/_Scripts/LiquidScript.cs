83e2ffa1b52dc833a77b57d389d9a60831e325ca
Ian Iversen-Krampitz <72539343+ianiversenkrampitz@users.noreply.github.com>
2025-03-12 00:10:42 -0700

    Ian Commit 11
    -improved water shader
    -made better prefabs
    -added WaterRenderer prefab

---
+++ gator_rade/Assets/_Scripts/LiquidScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiquidScript : MonoBehaviour
+{
+        public Material material; 
+        private Rigidbody rb;      
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            // Get the object's velocity from the Rigidbody
+            Vector3 velocity = rb.velocity;
+
+            // Convert the velocity to magnitude (speed)
+            float velocityMagnitude = (velocity.magnitude * .1f);
+
+            // Set the velocity magnitude and direction to the shader material property
+            material.SetFloat("_VelocityMagnitude", velocityMagnitude);
+            material.SetVector("_DisplacementDirection", velocity.normalized);
+        }
+}
