80d62f7def0c524c575bb8564450d80071a5346f
Jiokester <jiokerules@gmail.com>
2025-03-25 19:53:38 -0700

    Gravitate Liquid stuff too

--- gator_rade/Assets/_Scripts/GravitateLiquid.cs
+++ gator_rade/Assets/_Scripts/GravitateLiquid.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class GravitateLiquid : MonoBehaviour
 {
+    private Rigidbody _liquidRb;
     public GameObject target;
     public float moveSpeed;
-
+    public float radius;
+    public float power;
     private void Update()
     {
         if (target != null)
         {
             Vector3 newPosition = Vector3.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
             transform.position = newPosition;
+        }
+
+
+    }
+
+    void MoveBack()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider collider in hitColliders) 
+        {
+            if (_liquidRb != null)
+            _liquidRb.AddExplosionForce(power, transform.position, radius);
         }
     }
 }
