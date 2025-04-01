4b1e62d61f8f866aa957817f08eb37a2a2c0deaa
Ian Iversen-Krampitz <72539343+ianiversenkrampitz@users.noreply.github.com>
2025-04-01 08:46:57 -0700

    fixed gravitate liquid and added to all prefabs

--- gator_rade/Assets/_Scripts/GravitateLiquid.cs
+++ gator_rade/Assets/_Scripts/GravitateLiquid.cs
@@ -8,6 +8,7 @@ public class GravitateLiquid : MonoBehaviour
     public GameObject target;
     public float moveSpeed;
     public float radius;
+    public float deadZone;
     public float power;
 
 
@@ -18,16 +19,34 @@ public class GravitateLiquid : MonoBehaviour
             Vector3 newPosition = Vector3.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
             transform.position = newPosition;
         }
+        MoveBack();
     }
-
+    /// <summary>
+    /// for debug so you can see sphere radius
+    /// </summary>
+   /* void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green; // Set the color of the sphere
+        Gizmos.DrawWireSphere(transform.position, radius); // Draw the wireframe sphere
+        Gizmos.DrawWireSphere(transform.position, deadZone); // Draw the wireframe sphere
+    }
+   */
+   
     void MoveBack()
     {
+        //sets target back to nothing if theres nothing in range 
+        target = null;
+        //checks if theres objects in the radius 
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
 
         foreach (Collider collider in hitColliders)
         {
-            if (liquidRb != null)
-                liquidRb.AddExplosionForce(power, transform.position, radius); 
+            //checks if the distance is smaller than the deadzone, sets the target to other object if not and if it has the liquid tag 
+           if ((Vector3.Distance(collider.transform.position, transform.position) >= deadZone) && collider.CompareTag("Gatorade"))
+           {
+                target = collider.gameObject;
+                break;
+           }
         }
     }
 }
