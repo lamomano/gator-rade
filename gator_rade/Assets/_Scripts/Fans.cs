6f6b025381871436ced1dc5a920dda0e7f633b6d
lamomano <kanyonsr@gmail.com>
2025-04-09 21:40:27 -0700

    fans work now :D

---
+++ gator_rade/Assets/_Scripts/Fans.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fans : MonoBehaviour
+{
+
+    public float power = 5f;
+
+    public int distance = 8;
+    public bool isEnabled = false;
+
+
+
+    private void Awake()
+    {
+        // apply distance
+        //BoxCollider thisCollider = gameObject.AddComponent<BoxCollider>();
+        //thisCollider.isTrigger = true;
+        BoxCollider thisCollider = GetComponent<BoxCollider>();
+        // adding +1 so that it covers itself as well
+        thisCollider.size = new Vector3(0.85f, distance+1, 1);
+        thisCollider.center = new Vector3(0.12f, (distance / 2) , 0);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (isEnabled)
+        {
+            if (other.tag == "Gatorade" || other.tag == "Peak" || other.tag == "Powerade")
+            {
+                print("movin these liquids");
+                Rigidbody thisRb = other.GetComponent<Rigidbody>();
+                thisRb.AddForce(transform.up.normalized * power, ForceMode.Force);
+
+            }
+        }
+    }
+
+}
